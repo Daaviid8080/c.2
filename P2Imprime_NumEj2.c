adff907adcbe760fcaa445eb81c0e176d6a5a39c
Daaviid8080 <dalvap04@estudiantes.unileon.es>
2024-12-02 18:36:53 +0100

    Add files via upload

---
+++ P2Imprime_NumEj2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    int a;
+    printf("introducca un numero \n");
+    scanf("%d", &a);
+    printf("El número introducido es: %d \nFin del algoritmo \n" ,a);
+    return 0;
+}
